4483b380322379fb817ad71ae0227b7cb81fbe1b
Bavithran <sivabavithran16@gmail.com>
2024-12-25 14:19:37 +0530

    backend database storing cart items

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,14 +1,16 @@
 const orderModel = require('../models/orderModel')
 
-exports.createOrder = (req,res,next) => {
+exports.createOrder = async(req,res,next) => {
     try {
 
         const cartItems = req.body;
         const amount = Number(cartItems.reduce((acc,item)=>(acc+item.product.price * item.qty),0)).toFixed(2);
-        console.log(amount,'AMOUNT')
+        const status ='pending';
+
+        const order = await orderModel.create({cartItems, amount, status})
         res.json({
             success: true,
-            message: 'This is working'
+            order
         })
     } catch (error) {
         res.json({
