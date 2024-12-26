6744a1547cf20fe71cba223cd30631fc38ec202e
Bavithran <sivabavithran16@gmail.com>
2024-12-26 12:45:35 +0530

    increase decrease btn

--- frontend/src/pages/productDetail.js
+++ frontend/src/pages/productDetail.js
@@ -1,20 +1,45 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
-export default function () {
-    const [product, setProduct]=useState(null);
-    const {id} = useParams();
-    useEffect(() => {
-        fetch(process.env.REACT_APP_API_URL + '/product/' + id)
-            .then(res => res.json())
-            .then(res => {
-                console.log(res); // Log the API response for debugging
-                setProduct(res.product); // Update the state
-            })
-            .catch(err => console.error("Error fetching product:", err));
-    }, [id]);
+export default function ProductDetail({ cartItems, setCartItems }) {
+  const [product, setProduct] = useState(null);
+  const [qty, setqty] = useState(1);
+  const { id } = useParams();
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API_URL + "/product/" + id)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res); // Log the API response for debugging
+        setProduct(res.product); // Update the state
+      })
+      .catch((err) => console.error("Error fetching product:", err));
+  }, [id]);
+
+  function addtoCart() {
+    const itemExist = cartItems.find((item) => item.product._id == product._id);
+    if (!itemExist) {
+      const newItems = { product, qty };
+      setCartItems((state) => [...state, newItems]);
+      toast.success("Cart Items added Successfully");
+    }
+  }
+
+  function increaseQty() {
+    if(product.stock == qty){
+        return;
+    }
+    setqty((state) => state + 1);
+  }
+  function decreaseQty(){
+    if(qty > 1){
+        setqty((state) => state - 1);
+    }
     
-    return product && (
+  }
+
+  return (
+    product && (
       <div className="container container-fluid">
         <div className="row d-flex justify-content-around">
           <div className="col-12 col-lg-5 img-fluid" id="product_image">
@@ -25,63 +50,73 @@ export default function () {
               width="500"
             />
           </div>
-  
+
           <div className="col-12 col-lg-5 mt-5">
             <h3>{product.name}</h3>
             <p id="product_id">{product._id}</p>
-  
+
             <hr />
-  
+
             <div className="rating-outer">
-              <div className="rating-inner" style={{width:` ${product.ratings/5 * 100}%`}}></div>
+              <div
+                className="rating-inner"
+                style={{ width: ` ${(product.ratings / 5) * 100}%` }}
+              ></div>
             </div>
-  
+
             <hr />
-  
+
             <p id="product_price">${product.price}</p>
             <div className="stockCounter d-inline">
-              <span className="btn btn-danger minus">-</span>
-  
+              <span className="btn btn-danger minus" onClick={decreaseQty}>-</span>
+
               <input
                 type="number"
                 className="form-control count d-inline"
-                value="1"
+                value={qty}
                 readOnly
               />
-  
-              <span className="btn btn-primary plus">+</span>
+
+              <span className="btn btn-primary plus" onClick={increaseQty}>
+                +
+              </span>
             </div>
             <button
               type="button"
+              onClick={addtoCart}
               id="cart_btn"
               className="btn btn-primary d-inline ml-4"
             >
               Add to Cart
             </button>
-  
+
             <hr />
-  
+
             <p>
-              Status: <span id="stock_status" className={product.stock > 0 ? 'text-success' : 'text-danger'}>{product.stock > 0 ? 'In Stock' : 'Out of Stock'}</span>
+              Status:{" "}
+              <span
+                id="stock_status"
+                className={product.stock > 0 ? "text-success" : "text-danger"}
+              >
+                {product.stock > 0 ? "In Stock" : "Out of Stock"}
+              </span>
             </p>
-  
+
             <hr />
-  
+
             <h4 className="mt-2">Description:</h4>
-            <p>
-             {product.description}
-            </p>
-  
+            <p>{product.description}</p>
+
             <hr />
-  
+
             <p id="product_seller mb-3">
               Sold by: <strong>{product.seller}</strong>
             </p>
-  
+
             <div className="rating w-50"></div>
           </div>
         </div>
       </div>
-    );
-  }
-  
+    )
+  );
+}
